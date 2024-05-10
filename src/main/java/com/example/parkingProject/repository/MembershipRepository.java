ce99ce51d6e80151db680082d7523b91040f0b60
tj0404 <eunchany96@gmail.com>
2024-05-10 15:58:43 +0900

    #태준 회원조회 뷰 완성

---
+++ src/main/java/com/example/parkingProject/repository/MembershipRepository.java
@@ -0,0 +1,17 @@
+package com.example.parkingProject.repository;
+
+import com.example.parkingProject.entity.Membership;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MembershipRepository extends JpaRepository<Membership, Long> {
+    Page<Membership> findByNameContains(String keyword, Pageable pageable);
+
+    Page<Membership> findByPhoneContains(String keyword, Pageable pageable);
+
+    Page<Membership> findByCarNumberContains(String keyword, Pageable pageable);
+
+}
