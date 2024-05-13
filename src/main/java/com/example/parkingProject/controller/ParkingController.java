b9d3574b48a3ab8d202163e5cda2f5c762b46967
EonKim431 <sunbea0730@gmail.com>
2024-05-13 16:20:05 +0900

    #언 계산 merge오류 해결중

--- src/main/java/com/example/parkingProject/controller/ParkingController.java
+++ src/main/java/com/example/parkingProject/controller/ParkingController.java
@@ -1,12 +1,18 @@
 package com.example.parkingProject.controller;
 
+
+import com.example.parkingProject.dto.ParkingStateDto;
+import com.example.parkingProject.entity.ParkingState;
+import com.example.parkingProject.service.ParkingService;
+import jakarta.persistence.EntityManager;
+
 import com.example.parkingProject.constant.MembershipType;
 import com.example.parkingProject.dto.MemberDto;
-import com.example.parkingProject.dto.ParkingStateDto;
 import com.example.parkingProject.entity.Membership;
 import com.example.parkingProject.service.MemberService;
 import com.example.parkingProject.service.PaginationService;
-import com.example.parkingProject.service.ParkingService;
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -19,15 +25,58 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Controller
+@Slf4j
 public class ParkingController {
     @Autowired
-    MemberService memberService;
+    EntityManager em;
+
     @Autowired
     ParkingService parkingService;
+
+    @GetMapping("parkingState")
+    public String parkingState(Model model,
+                               @PageableDefault(page = 0, size = 10, sort = "stateId",
+                                       direction = Sort.Direction.ASC) Pageable pageable){
+        parkingService.currentPrice(); //currentPrice를 먼저 db에 저장해준 후 시작
+        Page<ParkingState> paging = parkingService.pagingList(pageable);
+        int totalPage = paging.getTotalPages();
+        List<Integer> barNumbers = parkingService.getPaginationBarNumbers(pageable.getPageNumber(), totalPage);
+        model.addAttribute("paginationBarNumbers", barNumbers);
+        model.addAttribute("paging", paging);
+
+        return "parking/parkingState";
+    }
+
+    @GetMapping("parkingState/search")
+    public String parkingStateSearch(@RequestParam("keyword")String keyword,
+                                     @RequestParam("searchType")String type,
+                                     Model model,
+                                     @PageableDefault(page = 0, size = 10, sort = "stateId",
+                                             direction = Sort.Direction.DESC) Pageable pageable){
+        Page<ParkingState> searchList = null;
+        List<Integer> barNumbers = null;
+        if(type.equals("carNumber")){
+            searchList = parkingService.searchByCarNumber(pageable,keyword);
+            int totalPage = searchList.getTotalPages();
+            barNumbers = parkingService.getPaginationBarNumbers(pageable.getPageNumber(), totalPage);
+        }
+        model.addAttribute("paginationBarNumbers", barNumbers);
+        model.addAttribute("paging", searchList);
+        //서칭+페이징을 위해 받아온 키워드와 검색타입도 넘김
+        model.addAttribute("keyword", keyword);
+        model.addAttribute("searchType", type);
+        return "parking/parkingState";
+    }
+
+
+    @Autowired
+    MemberService memberService;
     @Autowired
     PaginationService paginationService;
 
@@ -69,7 +118,6 @@ public class ParkingController {
     public String main(){
         return "main";
     }
-
 
     //회원 조회 페이지
     @GetMapping("viewMember")
@@ -159,15 +207,4 @@ public class ParkingController {
         return "redirect:/";
     }
 
-    @GetMapping("/payment")
-    public String payment(@RequestParam("id")Long id, Model model){
-        ParkingStateDto dto = parkingService.findById(id);
-        model.addAttribute("state",dto);
-        boolean member = memberService.findByCarNumber(dto.getCarNumber());
-        model.addAttribute("member",member);
-        System.out.println(dto);
-        return "parking/payment";
-    }
 }
-
-
