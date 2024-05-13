7a5724e2013c700034e9d99002c4b2f5ae763e3b
JooSeong12 <jsi50069@gmail.com>
2024-05-13 12:07:17 +0900

    #주성 정규표현식 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,4 @@
-insert into parking_state(car_number, in_time) values('11수0954', '2024-05-09 13:00:00');
+insert into parking_state(car_number, in_time) values('09수0954', '2024-05-09 13:00:00');
 insert into parking_state(car_number, in_time) values('12마8637', '2024-05-09 11:00:00');
 insert into parking_state(car_number, in_time) values('59무8874', '2024-05-09 10:00:00');
+insert into parking_state(car_number, in_time) values('101무8874', '2024-05-09 10:00:00');
